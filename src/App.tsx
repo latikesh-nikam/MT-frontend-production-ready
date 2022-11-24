7d77185950e5c612797183ab145801ff0049be87
nikhil-ladage <nikhil.ladage@coditas.com>
2022-11-24 17:55:54 +0530

    Minor code change

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 const App = () => {
   const [updatedData, setUpdateData] = useState();
+  const [updatedDataTo, setUpdateDataTo] = useState();
   useEffect(() => {}, []);
 
   return <>This is app</>;
